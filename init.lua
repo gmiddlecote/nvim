aa4f724bb242d9a9e1c1223f354b8e806e0c58de
George Middlecote <gmiddlecote@gmail.com>
2024-02-15 18:16:02 +0530

    chore: update dependencies for nvim plugins

--- init.lua
+++ init.lua
@@ -48,3 +48,25 @@ end
 --  " %{%v:lua.vim.fn.expand('%F')%}  %{%v:lua.require'nvim-navic'.get_location()%}"
 
 vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+
+local rt = require("rust-tools")
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set(
+        "n",
+        "<C-space>",
+        rt.hover_actions.hover_actions,
+        { buffer = bufnr }
+      )
+      -- Code action groups
+      vim.keymap.set(
+        "n",
+        "<Leader>a",
+        rt.code_action_group.code_action_group,
+        { buffer = bufnr }
+      )
+    end,
+  },
+})
